edd86e46daf6c314dcf9e1aa69a4b606086ec603
Dzeisonov <permanajason03@gmail.com>
2024-03-03 13:16:38 +0700

    Update Home Page

--- script.js
+++ script.js
@@ -15,3 +15,24 @@ function autoSlideShow() {
 autoSlideShow();
 
 setInterval(autoSlideShow, 4500);
+
+let showButton = document.getElementById("show-button");
+showButton.addEventListener("click", showOffers);
+
+function showOffers() {
+    let hiddenCard1 = document.getElementById("hidden-card1");
+    let hiddenCard2 = document.getElementById("hidden-card2");
+    let hiddenCard3 = document.getElementById("hidden-card3");
+    if(hiddenCard1.style.display == "none") {
+        hiddenCard1.style.display = "block";
+        hiddenCard2.style.display = "block";
+        hiddenCard3.style.display = "block";
+        showButton.textContent = "See less offers";
+    }
+    else {
+        hiddenCard1.style.display = "none";
+        hiddenCard2.style.display = "none";
+        hiddenCard3.style.display = "none";
+        showButton.textContent = "See all offers";
+    }
+}
